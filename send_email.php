e5ba17e6c3dbe997442dda069c643c6b99a95fcf
Marek Melichar <koprivackej@gmail.com>
2017-06-10 15:47:28 +0200

    for deploy

--- send_email.php
+++ send_email.php
@@ -1,41 +1,14 @@
 <?php
 
-echo ($_GET['json']);
+$request = $_GET['json'];
+$json = $request;
 
+$array;
 
+foreach ($request as $name => $value) {
+  $array[] = "$name: $value\n";
+}
 
+$data = implode("\n", $array);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//
-// $request = $_GET['json'];
-// $json = json_decode($request);
-//
-// $array;
-//
-// foreach ($request as $name => $value) {
-//     // $variable = "$name: $value\n";
-//     // array_push($array, "$name: $value\n")
-//
-//     $array[] = "$name: $value\n";
-// }
-//
-// $data = implode("\n", $array);
-//
-//
-//
-// mail( 'marek.melichar@centrum.cz', 'subject', $data, 'headers', 'attachments' );
+mail( 'marek.melichar@centrum.cz', 'subject', $data, 'headers', 'attachments' );
